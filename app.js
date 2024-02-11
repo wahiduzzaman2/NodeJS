0e575b61480f5698ca944d7d52f90918da38080a
wahiduzzaman2 <79000023+wahiduzzaman2@users.noreply.github.com>
2024-02-11 20:13:21 +0600

    node files

---
+++ app.js
@@ -0,0 +1,7 @@
+// const a = require("./index");
+
+
+// console.log(a)
+
+// a.average(10,20);
+// a.percent(20,100)
---
+++ index.js
@@ -0,0 +1,62 @@
+// console.log("hello World")
+
+
+////........... File Based
+
+// const a = 400;
+
+// console.log(a);
+
+// const a = {
+//     average: (a,b)=>{
+//         console.log((a+b)/2);
+
+//     },
+//     percent: (a,b)=>{
+//         console.log((a/b)*100);
+//     },
+// };
+
+// module.exports = a;
+
+
+////.........Build In
+
+// const fs = require("fs");
+
+// console.log(fs);
+
+// fs.readFile("./sample.txt", "utf-8", (err, data)=>{
+//     if(err) {
+
+//         throw err;
+
+//     }
+//     console.log(data);
+// });
+
+//Asynchronize
+
+//Let's do synchronize
+
+// const a = fs.readFileSync("./sample.txt", "utf-8");
+
+// console.log(a);
+// console.log("Hello");
+
+//.........path
+
+// const path = require("path")
+
+// const a = path.extname("/node/index.js");
+
+// console.log(a);
+
+
+///......os
+
+const os = require("os");
+
+console.log(os.freemem());
+console.log(os.hostname());
+console.log(os.totalmem());
