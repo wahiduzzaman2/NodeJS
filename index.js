cc4fea9200da9ff19aa3107defbac3e4ddfbd1b8
wahiduzzaman2 <79000023+wahiduzzaman2@users.noreply.github.com>
2024-02-12 02:49:32 +0600

    add .env

--- index.js
+++ index.js
@@ -62,6 +62,51 @@
 // console.log(os.totalmem());
 
 
-const pokemon = require("pokemon");
+// const pokemon = require("pokemon");
 
-console.log(pokemon.random());
+// console.log(pokemon.random());
+
+
+///...........Server create
+
+// import dotenv from "dotenv";
+
+const http = require("http");
+const fs = require("fs");
+const PORT = 5000;
+const hostname = "localhost";
+
+const home = fs.readFileSync("./index.html");
+
+const server = http.createServer((req, res) => {
+
+    if(req.url==="/"){
+        return res.end(home);
+    }
+    if(req.url==="/about"){
+        return res.end("<h1>About Page</h1>");
+    }
+    if(req.url==="/service"){
+        return res.end("<h1>Service Page</h1>");
+    }
+    if(req.url==="/contact"){
+        return res.end("<h1>contact Page</h1>");
+    }
+    else{
+        return res.end("<h1>404 Page Not Found</h1>");
+    }
+
+});
+
+// dotenv.config({
+//     path: "./config.env",
+//   });
+  
+
+server.listen(PORT,hostname,()=>{
+    console.log(`Server is working on http://${hostname}:${PORT}`);
+
+    // server.listen(process.env,PORT,()=>{
+    //     console.log(`Server is working on ${process.env.PORT}`);
+
+});
